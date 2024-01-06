6481caec89579693055d2ae381df779feb6bb0b8
kalibear99 <148329667+kalibear99@users.noreply.github.com>
2024-01-06 13:49:53 +0100

    update

--- portfolio/src/components/navbar/navbar.jsx
+++ portfolio/src/components/navbar/navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar() {
         <div className="right">
           <a href="#aboutme">O mně</a>
           <a href="#codelam">Co dělám</a>
+          <a href="#reference">Reference</a>
           <a href="#form">Kontaktovat</a>
         </div>
       </div>
--- portfolio/src/components/pages/fifth_page.jsx
+++ portfolio/src/components/pages/fifth_page.jsx
@@ -2,16 +2,22 @@ import "./fifth_page.css"
 
 export default function Reference() {
     return (
- <div class="landing-clients">
-        <h1 class="titleReference">Pracoval <span className="spanReference">jsme</span> například pro:</h1>
-        <div class="container">
-            <div class="landing-clients--loga">
-                    <div>
-                        <img className="logoReference" src="/Vector.png" alt="" />
+            <div id="reference" class="landing-clients">
+                <h1 class="titleReference">Pracoval <span className="spanReference">jsme</span> například pro:</h1>
+                <div class="container">
+                    <div class="loga">
+                            <div>
+                                <img className="logoReference" src="/csob.png" alt="" />
+                            </div>
+                            <div>
+                                <img className="logoReference" src="/glorious.png" alt="" />
+                            </div>
+                            <div>
+                                <img className="logoReference" src="/judo.png" alt="" />
+                            </div>
                     </div>
+                </div>
             </div>
-        </div>
-    </div>
 
     )   
 }
