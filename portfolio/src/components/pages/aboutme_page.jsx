034e1c7c7b03c168b990145fb8a0ce14b2166bcb
kalibear99 <148329667+kalibear99@users.noreply.github.com>
2024-01-12 10:19:10 +0100

    update

--- portfolio/src/components/pages/aboutme_page.jsx
+++ portfolio/src/components/pages/aboutme_page.jsx
@@ -1,44 +1,68 @@
 import "./aboutme_page.css"
 import 'animate.css';
+import {motion} from "framer-motion"
 
 
 
 export default function AboutMe() {
     return (
+
         <div id="aboutme">
-            <div className="aboutme_nadpis">
-                 <h1>O mně</h1>
-            </div>
-
-            <div className="aboutme_info">
-                <p>Ahoj, jmenuji se <span className="spanForm">Darien Kalist</span> a je mi 18 let. Momentálně studuji na <span className="spanForm">Střední škole Baltaci</span> ve Zlíně, kde se zaměřuji na zajímavý svět informatiky. Baví mě nejen digitální svět, ale také bojové sporty. Mimo školní život se věnuji <span className="spanForm">brazilskému jiu-jitsu</span>. Bojové sporty mi nejen <span className="spanForm">poskytují fyzickou sílu</span>, ale také mě učí disciplíně a strategii. Je to pro mě nejen sport, ale i forma <span className="spanForm"> sebepoznání a seberealizace</span>.</p>
-            </div>
-
-            <div className="container">
-                <div className="about">
-
-
-                    
-                    <div className="circle black">
-                        <div className="inner-circle green"></div>
-                    </div>
-                        <p className="text">Darien Kalist</p>
+            <motion.div
+                initial= {{scale: 1, opacity: 0, y: 50}}
+                whileInView={{opacity: 1, y: 0}}
+                transition={{duration: 0.5}}
+                viewport={{once: false}}
+            >
+                <div className="aboutme_nadpis">
+                    <h1>O mně</h1>
                 </div>
+            </motion.div>
 
-                <div className="about">
-                    <div className="circle black">
-                        <div className="inner-circle green"></div>
-                    </div>
-                        <p className="email">dory@gmail.com</p>
+            <motion.div 
+            initial= {{scale: 1, opacity: 0, y: 50}}
+            whileInView={{opacity: 1, y: 0}}
+            transition={{duration: 0.5}}
+            viewport={{once: false}}
+            >
+                <div className="aboutme_info">
+                    <p>Ahoj, jmenuji se <span className="spanForm">Darien Kalist</span> a je mi 18 let. Momentálně studuji na <span className="spanForm">Střední škole Baltaci</span> ve Zlíně, kde se zaměřuji na zajímavý svět informatiky. Baví mě nejen digitální svět, ale také bojové sporty. Mimo školní život se věnuji <span className="spanForm">brazilskému jiu-jitsu</span>. Bojové sporty mi nejen <span className="spanForm">poskytují fyzickou sílu</span>, ale také mě učí disciplíně a strategii. Je to pro mě nejen sport, ale i forma <span className="spanForm"> sebepoznání a seberealizace</span>.</p>
                 </div>
+            </motion.div>
 
-                <div className="about">
-                    <div className="circle black">
-                        <div className="inner-circle green"></div>
+            <motion.div
+             initial= {{scale: 1, opacity: 0, y: 50}}
+             whileInView={{opacity: 1, y: 0}}
+             transition={{duration: 0.5}}
+             viewport={{once: false}}
+            
+            >
+                <div className="container">
+                    <div className="about">
+
+
+                        
+                        <div className="circle black">
+                            <div className="inner-circle green"></div>
+                        </div>
+                            <p className="text">Darien Kalist</p>
                     </div>
-                        <p className="phone">123 456 789</p>
+
+                    <div className="about">
+                        <div className="circle black">
+                            <div className="inner-circle green"></div>
+                        </div>
+                            <p className="email">dory@gmail.com</p>
+                    </div>
+
+                    <div className="about">
+                        <div className="circle black">
+                            <div className="inner-circle green"></div>
+                        </div>
+                            <p className="phone">123 456 789</p>
+                    </div>
                 </div>
-            </div>
+            </motion.div>
             
         </div>
     );
